7d0db4a24976cf6e5dfe85bb310852976de4a3da
Willian Damasceno <williandamascenoprofessional@gmail.com>
2023-11-15 19:42:46 -0300

    Banco de dados

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # e-commerce
+
+## Criar o Banco de dados
+
+- Instalar o Postgres
+
+- Entrar na pasta **backend** pelo terminal
+
+```bash
+cd backend
+```
+
+- Para Criar o banco de dados execute o seguinte comando
+
+```bash
+psql -U postgres -d ecommerce -f db/tables.sql
+```
+
+- Para verificar se o banco existe execute o seguinte comando
+
+```bash
+psql -U postgres
+```
+
+- Digite a senha do Postgres e em seguida execute
+
+```bash
+\l
+```
+
+- Você verá a lista de banco de dados existentes
+
+## Deletar o Banco de dados
+
+- Desconectar totalmente do Postgres rodando os seguintes comandos
+
+```bash
+psql -U postgres
+
+\q
+```
+
+Em seguida você deve rodar a query que deleta o banco
+
+```bash
+psql -U postgres -f db/delete-db.sql
+```
---
+++ backend/db/tables.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE ecommerce;
+
+CREATE TABLE login (
+    usuario VARCHAR(255) PRIMARY KEY,
+    senha VARCHAR(255),
+    codigo_cliente INT,
+    FOREIGN KEY (codigo_cliente) REFERENCES cliente(codigo_cliente)
+);
+
+CREATE TABLE cliente (
+    codigo_cliente INT PRIMARY KEY,
+    nome VARCHAR(255),
+    rua VARCHAR(255),
+    numero VARCHAR(50),
+    complemento VARCHAR(255),
+    bairro VARCHAR(255),
+    cidade VARCHAR(255),
+    uf VARCHAR(2),
+    cep VARCHAR(10)
+);
+
+CREATE TABLE produtos (
+    codigo_produto INT PRIMARY KEY,
+    descricao VARCHAR(255),
+    preco DECIMAL(10, 2),
+    imagem TEXT
+);
+
+CREATE TABLE pedidos (
+    codigo_pedido INT PRIMARY KEY,
+    codigo_cliente INT,
+    total DECIMAL(10, 2),
+    FOREIGN KEY (codigo_cliente) REFERENCES cliente(codigo_cliente)
+);
+
+CREATE TABLE item_pedido (
+    codigo_pedido INT,
+    sequencial INT,
+    codigo_produto INT,
+    quantidade INT,
+    total_item DECIMAL(10, 2),
+    PRIMARY KEY (codigo_pedido, sequencial),
+    FOREIGN KEY (codigo_pedido) REFERENCES pedidos(codigo_pedido),
+    FOREIGN KEY (codigo_produto) REFERENCES produtos(codigo_produto)
+);
