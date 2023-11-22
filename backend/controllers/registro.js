e99889b11ab21de1682716231a23d61c716c186e
Willian Damasceno <williandamascenoprofessional@gmail.com>
2023-11-22 20:54:10 -0300

    Simplificação das funções de autenticação

--- backend/controllers/registro.js
+++ backend/controllers/registro.js
@@ -14,47 +14,31 @@ export const registroController = async (req, res) => {
     });
   }
 
-  try {
-    await client.query(
-      "INSERT INTO cliente (nome, rua, numero, complemento, bairro, cidade, uf, cep) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
-      [
-        body["nome"],
-        body["rua"],
-        body["numero"],
-        body["complemento"],
-        body["bairro"],
-        body["cidade"],
-        body["uf"],
-        body["cep"],
-      ]
-    );
-  } catch (e) {
-    console.log(e);
+  const cliente = await client.query(
+    `
+        INSERT INTO cliente (nome, rua, numero, complemento, bairro, cidade, uf, cep)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        RETURNING *;
+      `,
+    [
+      body["nome"],
+      body["rua"],
+      body["numero"],
+      body["complemento"],
+      body["bairro"],
+      body["cidade"],
+      body["uf"],
+      body["cep"],
+    ]
+  );
 
-    return res
-      .status(400)
-      .json({ mensagem: "Algo deu errado ao tentar cadastrar o usuário" });
-  }
+  await client.query(
+    `
+        INSERT INTO login (usuario, senha, codigo_cliente)
+        VALUES ($1, $2, $3)
+      `,
+    [body["usuario"], body["senha"], cliente.rows[0].codigo_cliente]
+  );
 
-  try {
-    const codigoUltimoClienteQuery = await client.query(
-      "SELECT codigo_cliente FROM cliente ORDER BY codigo_cliente DESC LIMIT 1"
-    );
-    const codigoUltimoCliente = codigoUltimoClienteQuery.rows[0].codigo_cliente;
-
-    await client.query(
-      "INSERT INTO login (usuario, senha, codigo_cliente) VALUES ($1, $2, $3)",
-      [body["usuario"], body["senha"], codigoUltimoCliente]
-    );
-
-
-
-    return res.status(200).json({ mensagem: "Usuário registrado com sucesso" });
-  } catch (e) {
-    console.log(e);
-
-    return res
-      .status(400)
-      .json({ mensagem: "Algo deu errado ao tentar cadastrar o usuário" });
-  }
+  return res.status(200).json({ cliente: cliente.rows[0] });
 };
